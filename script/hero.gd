97494aea2b7b7e5c24f72bafa37f044900fca049
Abdelrhman <abdelrazkforman966@gmail.com>
2026-08-24 16:00:34 +0300

    finishing the right and left moving with animation and i am laging now because the hero is bouncing on the static body i put so i watching some videos and try to solve this problem

--- script/hero.gd
+++ script/hero.gd
@@ -2,16 +2,24 @@ extends CharacterBody2D
 
 # the variables i will use for the movement of the character
 @export var speed = 300
-@export var gravity = 900
+@export var gravity = 1200
 @export var jump_force = 255
 
 func _physics_process(delta):
 	
 	var direction = Input.get_axis("left", "right")
-	
 	if direction:
 		velocity.x = direction * speed
+		$AnimatedSprite2D.play("run")
 	else:
 		velocity.x = 0
+		$AnimatedSprite2D.play("idle")
 
+	#gravity of the hero it will be seen when i build the map
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		velocity.y = 0
+		
+		
 	move_and_slide()
