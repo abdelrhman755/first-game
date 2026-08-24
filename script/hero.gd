3f76f3db07978b25817d6ca66883aa5668ac084e
Abdelrhman <abdelrazkforman966@gmail.com>
2026-08-24 19:24:31 +0300

    changes the sene of the hero and put it in the world sene to make the hero can stand on the ground (it take much time from me because it is my first game in life) now i will start at the level one and build its background and world"

--- script/hero.gd
+++ script/hero.gd
@@ -2,18 +2,20 @@ extends CharacterBody2D
 
 # the variables i will use for the movement of the character
 @export var speed = 300
-@export var gravity = 1200
-@export var jump_force = 255
+@export var gravity = 9000
+@export var jump_force = 2000
 
 func _physics_process(delta):
 	
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * speed
-		$AnimatedSprite2D.play("run")
+		if is_on_floor():
+			$AnimatedSprite2D.play("run")
 	else:
 		velocity.x = 0
-		$AnimatedSprite2D.play("idle")
+		if is_on_floor():
+			$AnimatedSprite2D.play("idle")
 
 	#gravity of the hero it will be seen when i build the map
 	if not is_on_floor():
@@ -21,5 +23,16 @@ func _physics_process(delta):
 	else:
 		velocity.y = 0
 		
+	
+	# the direction of the moving character
+	if direction == -1:
+			$AnimatedSprite2D.flip_h = true
+			
+	elif direction == 1:
+		$AnimatedSprite2D.flip_h = false
 		
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y -= jump_force
+		$AnimatedSprite2D.play("jump")
 	move_and_slide()
